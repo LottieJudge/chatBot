501243f24c2b792c6c3af806c6a295182cfff42c
lottiejudge <lottielucyjudge@gmail.com>
2025-10-06 20:56:45 +0100

    lmabda cont

--- lambda.py
+++ lambda.py
@@ -8,3 +8,6 @@ square_l = lambda x: x ** 2
 
 print(square(4))
 print(square_l(4))
+
+#  Lambda functions generally work as arguments with higher order functions such as map(), filter() 
+# Higher order functions are functions built in to python that will already do specific tings to data without having to explicility write the function.
