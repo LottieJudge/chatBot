f569548f051401e2b73839f6c3af920e8cbb405e
lottiejudge <lottielucyjudge@gmail.com>
2025-10-06 21:00:52 +0100

    map

--- lambda.py
+++ lambda.py
@@ -10,4 +10,11 @@ print(square(4))
 print(square_l(4))
 
 #  Lambda functions generally work as arguments with higher order functions such as map(), filter() 
-# Higher order functions are functions built in to python that will already do specific tings to data without having to explicility write the function.
+# Higher order functions are functions built in to python that will already do specific things to data without having to explicitly write the function.
+
+nums = [1,2,3,4,5,6]
+print(list(nums))
+print(list(map(lambda x: x + 1, nums)))
+# interesting!! Love map - why the heck haven't i been using this?
+squared = list(map( lambda x: x**2, nums))
+print(squared)
