71cebbba4f71c500a650049b081b994f8cdf4a36
lottiejudge <lottielucyjudge@gmail.com>
2025-10-07 06:48:23 +0100

    map

--- map.py
+++ map.py
@@ -1,1 +1,17 @@
-Map()
+# Map() - Map is a built in function that works likes a for loop, applying a given set of instructions to each item in an iterable and returns the new iterable. 
+
+#  Syntax:  map(function, iterable, [iterable2, iterable3])
+
+def double(x):
+  return x * 2
+
+nums = [1, 2, 3, 4, 5, 8]
+
+dubbed = map(double, nums)
+print(list(dubbed)) #Use list to be able to print out the results 
+
+def squared(x):
+  return x ** 2
+
+sqd = map(squared, nums)
+print(list(sqd))
