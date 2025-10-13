f1a05775434e8a50d8acbb79e1e7a4107b982825
lottiejudge <lottielucyjudge@gmail.com>
2025-10-13 18:00:52 +0100

    elif

--- coffeBot.py
+++ coffeBot.py
@@ -1,6 +1,12 @@
 def coffee_bot():
   print("Welcome to the cafe!")
   size = get_size()
+  if(size == 'a'):
+    size = 'Small'
+  elif(size == 'b'):
+    size = 'Medium'
+  elif(size == 'c'):
+    size = 'Large'
   print(size)
 
 def get_size():
