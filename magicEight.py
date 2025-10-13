0a010a5df1e7caea906693fdeab142ba57f9622c
lottiejudge <lottielucyjudge@gmail.com>
2025-10-13 15:22:03 +0100

    eiight ball

--- magicEight.py
+++ magicEight.py
@@ -2,5 +2,28 @@ import random
 
 name = "Scotty"
 question = input('Do you have a question for me child? ')
-Answer = " "
+answer = " "
 random_int = random.randint(1,9)
+
+if(random_int == 1):
+  answer = "Yes - Definitely"
+elif(random_int == 2):
+  answer = "It is decidedly so"
+elif(random_int == 3):
+  answer = "Without a doubt"
+elif(random_int == 4):
+  answer = "Reply hazy, Try again"
+elif(random_int == 5):
+  answer = "Ask again later"
+elif(random_int == 6):
+  answer = "Better not tell you now"
+elif(random_int == 7):
+  answer = "My sources say so"
+elif(random_int == 8):
+  answer = "Outlook not so good"
+elif(random_int == 9):
+  answer = "Very Doubtful"
+else: 
+  print("Error")
+
+print(answer)
