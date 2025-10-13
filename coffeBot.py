9a52b2698b3bcabb069a812be27eac3d0c45bbd2
lottiejudge <lottielucyjudge@gmail.com>
2025-10-13 15:54:16 +0100

    coffeeee bot

--- coffeBot.py
+++ coffeBot.py
@@ -1,1 +1,10 @@
-def coffee_bot()
+def coffee_bot():
+  print("Welcome to the cafe!")
+  size = get_size()
+  print(size)
+
+def get_size():
+  res = input("What size drink would you like? \n[a] small \n[b] medium \n[c] large \n")
+  return res
+
+coffee_bot()
