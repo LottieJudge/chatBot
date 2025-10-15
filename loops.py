e65966eaa7f6277109cf2034d1d62bca7045dcdb
lottiejudge <lottielucyjudge@gmail.com>
2025-10-15 11:41:06 +0100

    printing elements in a 2d list

--- loops.py
+++ loops.py
@@ -9,3 +9,9 @@ print(squares)
 
 cubes = [digit ** 3 for digit in single_digits]
 print(cubes)
+
+# printing every element of a 2d list
+grouped_topics = [ ["Algorithms", "Data Structures", "AI"], ["Linear Regression", "SQL"]]
+for sublist in grouped_topics:
+  for sublist_el in sublist:
+    print(sublist_el)
