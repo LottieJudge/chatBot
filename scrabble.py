f70b8c3ac21bf6156e5c693d6b2572a7ecd25c3a
lottiejudge <lottielucyjudge@gmail.com>
2025-10-18 18:17:57 +0100

    editing zip to make it work

--- scrabble.py
+++ scrabble.py
@@ -2,7 +2,7 @@ letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
 
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
-letters_to_points = zip(letters, points)
+letters_to_points = list(zip(letters, points))
 print(letters_to_points)
 letters_to_points.append([" ", 0])
 
@@ -10,4 +10,5 @@ def score_word(word):
   point_total = 0
   for letter in word:
     if letter in letters_to_points:
-      point_total + letters_to_points
+      point_total += letters_to_points
+    
